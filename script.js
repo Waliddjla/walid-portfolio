167055139c2abd9b8859cd4ccba805d641edfd76
Walid Djellali <walidbibo1997@gmail.com>
2023-05-04 13:56:40 +0100

    adding background

--- script.js
+++ script.js
@@ -31,3 +31,18 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
   
+function ReadMore () {
+    const dots = document.getElementById("dots");
+    const moreText = document.getElementById("more");
+    const btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+      }
+}
